f7ae515b16bfb1fac94b46762cfc20a91628d94e
higo <higoedu@gmail.com>
2023-07-01 23:49:44 -0300

    declarou variavel somou imprimiu
    declarou variavel somou imprimiu

---
+++ p_01_declarou_variavel_somou_imprimiu/main.js
@@ -0,0 +1,14 @@
+//declara 3 variáveis
+let a = 5, b = 10, c = 15;
+
+//soma as 3 variáveis e imprime na tela
+console.log(a + b + c);
+
+//ou no terminal do vscode, digita o código javascript, clear empurra para cima os comamndos
+//ou com node instalado, executa o arquivo js, digita node e o nome do arquivo com a extensão js
+
+/*
+develop tool - ferramenta de desenvolvimento, exemplo no chrome, roda javascript do lado do cliente
+    console
+node, roda javascript no meu computador
+*/
